e0d0e11b2d92f41b0982bb7f2a0e7a94735122a2
DFIR Matt <44217373+cerkah@users.noreply.github.com>
2023-07-19 15:43:54 -0400

    Create pesdemor2.ps1

---
+++ pesdemor2.ps1
@@ -0,0 +1,7 @@
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand JABFAG4AdgA6AEMATwBNAFAAVQBUAEUAUgBOAEEATQBFAA==
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand JABFAG4AdgA6AFUAUwBFAFIARABPAE0AQQBJAE4A
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand RwBlAHQALQBOAGUAdABJAFAAQwBvAG4AZgBpAGcAdQByAGEAdABpAG8AbgA=
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand RwBlAHQALQBMAG8AYwBhAGwAdQBzAGUAcgAgAHwAIABTAGUAbABlAGMAdAAgACoAIAB8ACAARgBvAHIAbQBhAHQALQBMAGkAcwB0ACAALQBQAHIAbwBwAGUAcgB0AHkAIAAqAA==
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand dwBoAG8AYQBtAGkAIAAvAHAAcgBpAHYA
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand bgBlAHQAIAB1AHMAZQByACAALwBkAG8AbQBhAGkAbgA=
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand bgBlAHQAIABnAHIAbwB1AHAAIAAcIEQAbwBtAGEAaQBuACAAQQBkAG0AaQBuAHMAHSAgAC8AZABvAG0AYQBpAG4A
