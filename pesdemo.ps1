3c7fc84e27cf03c5dc9b18b473013949cd94ca56
DFIR Matt <44217373+cerkah@users.noreply.github.com>
2023-07-18 23:12:12 -0400

    Create pesdemo.ps1
    Draft only

---
+++ pesdemo.ps1
@@ -0,0 +1,8 @@
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand dwBoAG8AYQBtAGkA
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand dwBoAG8AYQBtAGkAIAAvAHUAcwBlAHIA
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand dwBoAG8AYQBtAGkAIAAvAGEAbABsAA==
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand dwBoAG8AYQBtAGkAIAAvAGcAcgBvAHUAcABzAA==
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand dwBoAG8AYQBtAGkAIAAvAHAAcgBpAHYA
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand bgBlAHQAIAB1AHMAZQByAA==
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand bgBlAHQAIAB1AHMAZQByACAALwBkAG8AbQBhAGkAbgA=
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -EncodedCommand bgBlAHQAIABnAHIAbwB1AHAAIAAiAEQAbwBtAGEAaQBuACAAQQBkAG0AaQBuAHMAIgAgAC8AZABvAG0AYQBpAG4A
